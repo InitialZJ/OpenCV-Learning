d914c9c9938b6ea0829abd60e178c765bfec473a
Jeffrey0418 <blueskyzzz0418@gmail.com>
2022-06-06 13:04:27 +0800

    fix show image

--- 002_show_image.py
+++ 002_show_image.py
@@ -1,6 +1,11 @@
 import cv2
 
 img = cv2.imread('lena.jpg', 0)  # 灰度化读取
+# cv2.imshow('img', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+cv2.namedWindow('img', cv2.WINDOW_NORMAL)  # 指定WINDOW_NORMAL窗口，可拉伸
 cv2.imshow('img', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
